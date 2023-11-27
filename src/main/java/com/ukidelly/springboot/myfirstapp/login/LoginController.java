f719c75309dd9293b58b59b0c6f3705ff705ba4e
UkiDelly <daehyeon1999@naver.com>
2023-11-27 19:36:52 +0900

    🎨 로깅

--- src/main/java/com/ukidelly/springboot/myfirstapp/login/LoginController.java
+++ src/main/java/com/ukidelly/springboot/myfirstapp/login/LoginController.java
@@ -1,18 +1,27 @@
 package com.ukidelly.springboot.myfirstapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
 
     @GetMapping
-    public String loginView(Model model) {
-        
+    public String loginView(Model model, @RequestParam String name) {
+        logger.warn("name : {}", name);
+
+        // thymeleaf model에 name 값 추가
+        model.addAttribute("name", name);
+
         return "login";
     }
 
