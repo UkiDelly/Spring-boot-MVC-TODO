60ed774cbb704ab1ca9dfcec0092f378ab50e766
UkiDelly <daehyeon1999@naver.com>
2023-11-30 17:20:36 +0900

    ✨ Spring Security에서 H2에 접근 할수 있도록 커스텀

--- src/main/java/com/ukidelly/springboot/myfirstapp/SpringSecurityConfiguration.java
+++ src/main/java/com/ukidelly/springboot/myfirstapp/SpringSecurityConfiguration.java
@@ -3,11 +3,15 @@ package com.ukidelly.springboot.myfirstapp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -18,11 +22,11 @@ public class SpringSecurityConfiguration {
     public InMemoryUserDetailsManager createUserDetailsManager() {
         var adminUser = createNewUser("admin", "admin");
         var testUser = createNewUser("test", "test");
-        
+
         return new InMemoryUserDetailsManager(adminUser, testUser);
     }
 
-
+    // 새로운 유저를 생성하는 함수
     private UserDetails createNewUser(String username, String password) {
         return User.builder().passwordEncoder(input -> passwordEncoder().encode(input)).username(username).password(password).roles("USER", "ADMIN").build();
     }
@@ -34,4 +38,23 @@ public class SpringSecurityConfiguration {
     }
 
 
+    // H2 콘솔 url을 SpringSecurity에서 해제
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // 모든 요청을 승인
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        // 승인되지 않은 요청이 오면, formLogin을 호출
+        http.formLogin(withDefaults());
+
+        // csrf 비활성화
+        http.csrf().disable();
+
+        // frame option 비활성화
+        http.headers().frameOptions().disable();
+
+
+        return http.build();
+    }
 }
